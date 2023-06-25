a6a859430a7922370fb1bacdeaa02fce08c78318
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-06-25 14:46:37 +0530

    updated html css

--- script.js
+++
@@ -1,8 +0,0 @@
-let input=document.querySelector('.enter-task')
-let addbtn=document.querySelector('.add-bttn');
-let task=document.querySelector('.task');
-input.addEventListener('keyup',()=>{
-    if(input.value !=""){
-        addbtn.classList.add('active');
-    }
-})
